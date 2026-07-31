a8f2f1ac2d0fceae34951bb11c24ae3fdb192fdd
Mateus Pereira <53189625+mth-prog@users.noreply.github.com>
2026-07-31 17:54:37 -0300

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        """
+        t = jam
+        s = amj
+
+        dict() = {j:1, a:1, m:1}
+
+        test = {"j":1, "a":1, "m":1}
+        test1 = {"j":1, "a":1, "m":1}
+
+        isso é true
+        """
+
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0) # 1 + o valor se encontrar se nao é 0
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        
+        return countS == countT
